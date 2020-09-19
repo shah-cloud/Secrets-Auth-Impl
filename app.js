dde9aaaedf781a52da0a335e2cb7b43d65a44d84
shah-cloud <shahfahed.gla_cs15@gla.ac.in>
2020-09-19 08:05:58 +0530

    Simple login created successfully

---
+++ app.js
@@ -0,0 +1,78 @@
+//jshint esversion:6
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/userdb', {useNewUrlParser: true,  useUnifiedTopology: true});
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+// tis is very imp to get values from post req.
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String
+})
+
+const User = mongoose.model("User",userSchema);
+
+
+
+app.get("/",function (req,res){
+  res.render("home");
+});
+
+app.get("/login",function (req,res){
+  res.render("login");
+});
+
+app.get("/logout",function (req,res){
+  res.redirect("/");
+});
+
+app.get("/register",function (req,res){
+  res.render("register");
+});
+
+app.post("/register",function (req, res){
+
+  const newUser = new User({
+    email: req.body.username,
+    password: req.body.password
+  });
+
+  newUser.save(function(err){
+    if(err){
+      res.send(err);
+    }else {
+      res.render("secrets");
+    }
+  });
+
+});
+
+app.post("/login",function(req, res){
+  const email= req.body.username;
+  const password= req.body.password;
+
+  User.findOne({email: email},function(err,foundUser){
+    if (err){
+      res.send(err);
+    }else {
+      console.log(foundUser);
+      if (foundUser.password === password) {
+        res.render("secrets");
+      }else {
+        res.send("invalid password");
+      }
+    }
+  });
+});
+
+app.listen(3000, function (){
+  console.log("Server started on port 3000");
+})
