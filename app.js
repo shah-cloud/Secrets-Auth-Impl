fa1b465377963bf6de2ce5883ef693985c6cd282
shah-cloud <shahfahed.gla_cs15@gla.ac.in>
2020-09-20 11:08:28 +0530

    Successfully Bcrypt added

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ require('dotenv').config()
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require('mongoose');
-var md5 = require('md5');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 mongoose.connect('mongodb://localhost/userdb', {useNewUrlParser: true,  useUnifiedTopology: true});
 
@@ -41,34 +42,38 @@ app.get("/register",function (req,res){
 
 app.post("/register",function (req, res){
 
-  const newUser = new User({
-    email: req.body.username,
-    password: md5(req.body.password)
-  });
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+    const newUser = new User({
+      email: req.body.username,
+      password: hash
+    });
 
-  newUser.save(function(err){
-    if(err){
-      res.send(err);
-    }else {
-      res.render("secrets");
-    }
+    newUser.save(function(err){
+      if(err){
+        res.send(err);
+      }else {
+        res.render("secrets");
+      }
+    });
   });
 
 });
 
 app.post("/login",function(req, res){
   const email= req.body.username;
-  const password= md5(req.body.password);
+  const password= req.body.password;
 
   User.findOne({email: email},function(err,foundUser){
     if (err){
       res.send(err);
     }else {
-      if (foundUser.password === password) {
-        res.render("secrets");
-      }else {
-        res.send("invalid password");
-      }
+      bcrypt.compare(password, foundUser.password, function(err, result) {
+        if (result == true) {
+          res.render("secrets");
+        }else {
+          res.send("invalid password");
+        }
+      });
     }
   });
 });
